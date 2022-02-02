afda877fbea39bac4fbd69a6a613ab4d3abe9893
Patricia Jamriskova <patricia.jamriskova01@dixonscarphone.com>
2022-02-02 16:23:25 +0100

    fix comments in customFetch file

--- src/customFunctions/customFetch.ts
+++ src/customFunctions/customFetch.ts
@@ -1,25 +1,32 @@
 import { bearerTokenAuthorization } from "./../configs/bearerTokenAuthorization";
 
-export const customFetch = async (
+type CustomFetch<TResponseJson> = {
+  fullResponse: Response;
+  json: TResponseJson;
+};
+
+export const customFetch = async <TResponseJson>(
   input: RequestInfo,
   init: RequestInit = {}
-) => {
-  init.headers = init.headers || bearerTokenAuthorization.headers;
+): Promise<CustomFetch<TResponseJson>> => {
+  init.headers = Object.assign(bearerTokenAuthorization.headers, init.headers);
   const response = await fetch(input, init);
 
-  if (!response.ok) {
-    throw new CustomFetchError("Some problem with response: ", response);
-  }
+  if (!response.ok) throw new HttpResponseError("Some problem with response: ", response);
 
   return {
-    /*  NOTE: clone() method creates a copy. Both methods of creating a copy will fail if the body of the original request or response has already been read, but reading the body of a cloned response or request will not cause it to be marked as read in the original.
-     https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch */
+    /*  NOTE: clone() method creates a copy. 
+        Both methods of creating a copy will fail if the body of the original request 
+        or response has already been read, but reading the body of a cloned response
+        or request will not cause it to be marked as read in the original.
+        https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+     */
     fullResponse: response.clone(),
     json: await response.json(),
   };
 };
 
-class CustomFetchError extends Error {
+export class HttpResponseError extends Error {
   constructor(message: string, public readonly response: Response) {
     super(message);
   }
